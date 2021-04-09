e0fdbc08e93ff1e1a5b2f34486ffd29209c59c60
wengzepeng <wengzepeng@pcitech.com>
2021-04-09 18:14:00 +0800

    初始化

---
+++ VisualPlatform_Control/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualPlatform_Control
+{
+    public class Class1
+    {
+    }
+}
